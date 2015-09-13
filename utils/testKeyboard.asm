9d720d008ef2349c710a5178bf1e609f91f1b81a
MForever78 <shunjian1128@gmail.com>
2015-09-13 23:20:22 +0800

    modify keyboard test: using keyboard counter

--- utils/testKeyboard.asm
+++ utils/testKeyboard.asm
@@ -5,10 +5,13 @@
 		add 	$s2, $zero, $zero	# s2 init
 		lui 	$s2, 0x1000		# s2 saves the 7 seg addr
 		add 	$t0, $zero, $zero	# t0 is the keyboard counter
-wait_kbd:	lw	$t1, 0($s1)		# t1 is the ascii code
-		beq	$t1, $zero, wait_kbd
-		add 	$t2, $s1, $t0		# t2 is the addr we want to write
-		sw	$t1, 0($t2)		# write to screen
-		sw 	$t1, 0($s2)		# write to 7 seg
+		lw 	$t3, 0($s1)		# t3 is the comparator
+wait_kbd:	lw	$t1, 0($s1)		# t1 is the keyboard code
+		beq	$t1, $t3, wait_kbd
+		srl	$t4, $t1, 24		# t4 is the ascii code
+		add 	$t2, $s0, $t0		# t2 is the addr we want to write
+		sw	$t4, 0($t2)		# write to screen
+		sw 	$t4, 0($s2)		# write to 7 seg
 		addi	$t0, $t0, 1		# counter add one
+		add	$t3, $zero, $t1		# set comparator
 		j 	wait_kbd		# dead loop
